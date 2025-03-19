3473383031edf89bc760e42be3409a7988d1918e
Caden Kim <hyperdx1@gmail.com>
2025-03-19 18:46:28 +0900

    m

--- crates/crackle-kit/src/macros.rs
+++ crates/crackle-kit/src/macros.rs
@@ -1,18 +1,20 @@
+use paste::paste;
+
 /// # Example
 /// ```ignore
-/// 
+///
 /// trait DataTrait {
 ///     fn merge(&mut self, rhs:Self);
-/// 
+///
 ///     fn process(&self, string: &mut String);
 /// }
-/// 
+///
 /// struct AData;
 /// impl DataTrait for AData {
 ///     fn merge(&mut self, rhs:Self) {
 ///         // ...
 ///     }
-/// 
+///
 ///     fn process(&self, string: &mut String) {
 ///         // ...
 ///     }
@@ -22,7 +24,7 @@
 ///     fn merge(&mut self, rhs:Self) {
 ///         // ...
 ///     }
-/// 
+///
 ///     fn process(&self, string: &mut String) {
 ///         // ...
 ///     }
@@ -31,18 +33,19 @@
 ///     data: Vec<&'a str>,
 ///     num: T,
 /// }
-/// 
+///
 /// impl<'a,T> DataTrait for CData<'a, T> {
 ///     fn merge(&mut self, rhs:Self) {
 ///         // ...
 ///     }
-/// 
+///
 ///     fn process(&self, string: &mut String) {
 ///         // ...
 ///     }
 /// }
-/// 
+///
 /// gen_macros_to_impl_bdt_for_enum!(
+///     name_alias: bdt,
 ///     impl_start: {
 ///         impl<'a, T> Data<'a, T>
 ///     }
@@ -50,31 +53,31 @@
 ///         B(BData),
 ///         C(CData) // note that you need to remove generics
 /// );
-/// 
+///
 /// enum Data<'a, T> {
 ///     A(AData),
 ///     B(BData),
 ///     C(CData<'a, T>),
 /// }
-/// 
+///
 /// impl<'a, T> DataTrait for Data<'a, T> {
 ///     fn merge(&mut self, rhs:Self) {
 ///         gen_method_for_bdt_rhs!(self, merge, rhs=rhs)
 ///     }
-/// 
+///
 ///     fn process(&self, string: &mut String) {
 ///         gen_method_for_bdt!(self, string)
 ///     }
 /// }
-/// 
-/// 
+///
+///
 /// ```
-macro_rules! gen_macros_to_impl_bdt_for_enum {
-    (impl_start: {$($impl_start:tt)+}, $($variant:ident($field_type:ident)),+) => {
-        gen_macros_to_impl_bdt_for_enum!(@impl impl_start: {$($impl_start)+}, $($variant($field_type)),+, @dol=$);
+macro_rules! gen_macros_to_impl_trait_for_enum {
+    (name_alias: $enum_name_alias:ident, impl_start: {$($impl_start:tt)+}, $($variant:ident($field_type:ident)),+) => {
+        gen_macros_to_impl_trait_for_enum!(@impl name_alias: $enum_name_alias, impl_start: {$($impl_start)+}, $($variant($field_type)),+, @dol=$);
     };
 
-    (@impl impl_start: {$($impl_start:tt)+}, $($variant:ident($field_type:ident)),+, @dol=$dol:tt) => {
+    (@impl name_alias: $enum_name_alias:ident, impl_start: {$($impl_start:tt)+}, $($variant:ident($field_type:ident)),+, @dol=$dol:tt) => {
         // inner marro to define some array types. for example: const var_names:[&str; count!($($variant)+)] = ...;
         macro_rules! _count {
             () => (0usize);
@@ -91,59 +94,66 @@ macro_rules! gen_macros_to_impl_bdt_for_enum {
             }
         }
 
-        gen_macros_to_impl_bdt_for_enum!(
-            @method_macros $($variant),+, @dol=$
+        gen_macros_to_impl_trait_for_enum!(
+            @method_macros $enum_name_alias, $($variant),+, @dol=$
         );
     };
 
-    (@method_macros $($variant:ident),+, @dol=$dol:tt) => {
-        /// # Example
-        /// ```ignore
-        /// fn process(&self, a:i32, b:i32) -> i32 {
-        ///     gen_method_for_bdt!(self, a, b)
-        /// }
-        /// ```
-        macro_rules! gen_method_for_bdt {
-            ($enum_value:ident, $method_ident:ident$dol(,)? $dol($args:ident),*) => {
-                match $enum_value {
-                    $(
-                        Self::$variant(d) => {
-                            d.$method_ident($dol($args),*)
-                        }
-                    ),*
-                }
-            };
+    (@method_macros $enum_name_alias:ident, $($variant:ident),+, @dol=$dol:tt) => {
+
+        paste! {
+            /// # Example
+            /// ```ignore
+            /// fn process(&self, a:i32, b:i32) -> i32 {
+            ///     gen_method_for_bdt!(self, a, b)
+            /// }
+            /// ```
+            macro_rules! [<gen_method_for_ $enum_name_alias>] {
+                ($enum_value:ident, $method_ident:ident$dol(,)? $dol($args:ident),*) => {
+                    match $enum_value {
+                        $(
+                            Self::$variant(d) => {
+                                d.$method_ident($dol($args),*)
+                            }
+                        ),*
+                    }
+                };
+            }
         }
 
-        /// # Example
-        /// ```ignore
-        /// fn merge(&self, rhs:Self, b:i32) -> i32 {
-        ///     gen_method_for_bdt_rhs!(self, rhs=rhs, b)
-        /// }
-        /// ```
-        macro_rules! gen_method_for_bdt_rhs {
-            ($enum_value:ident, $method_ident:ident, rhs=$rhs:ident$dol(,)? $dol($args:ident),*) => {
-                match ($enum_value, $rhs) {
-                    $(
-                        (Self::$variant(d), Self::$variant(rhs)) => {
-                            d.$method_ident(rhs, $dol($args),*)
+        paste! {
+            /// # Example
+            /// ```ignore
+            /// fn merge(&self, rhs:Self, b:i32) -> i32 {
+            ///     gen_method_for_bdt_rhs!(self, rhs=rhs, b)
+            /// }
+            /// ```
+            macro_rules! [<gen_method_for_ $enum_name_alias _rhs>] {
+                ($enum_value:ident, $method_ident:ident, rhs=$rhs:ident$dol(,)? $dol($args:ident),*) => {
+                    match ($enum_value, $rhs) {
+                        $(
+                            (Self::$variant(d), Self::$variant(rhs)) => {
+                                d.$method_ident(rhs, $dol($args),*)
+                            }
+                        ),*,
+                        (lhs,rhs) => {
+                            panic!("Mismatched enum variants, lhs: {}, rhs: {}", lhs.variant_name(), rhs.variant_name());
                         }
-                    ),*,
-                    (lhs,rhs) => {
-                        panic!("Mismatched enum variants, lhs: {}, rhs: {}", lhs.variant_name(), rhs.variant_name());
                     }
-                }
-            };
+                };
+            }
         }
+        
+
 
     };
 }
 
-gen_macros_to_impl_bdt_for_enum!(
+gen_macros_to_impl_trait_for_enum!(
+    name_alias: bdt,
     impl_start: {
         impl BDT
-    }, 
-
+    },
     Kmer(Kmer),
     BaseCount(BaseCount),
     FragmentSize(FragmentSize),
